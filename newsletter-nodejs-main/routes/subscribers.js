7cf61e3cd5f7267a7b986eeb43f4051a17c1a234
KEERTHIVAASEN V <84181432+KEERTHIVAASEN@users.noreply.github.com>
2024-04-07 01:51:43 +0000

    Remove unused files and code

--- newsletter-nodejs-main/routes/subscribers.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const subscribersController = require('../controllers/subscribers');
-
-router.post('/subscribe', subscribersController.createSubscribers);
-router.post('/unsubscribe', subscribersController.deleteSubscribers);
-
-module.exports = router;
